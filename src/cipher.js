e68880bedb6e15ca56e1d9346c69f2f85fbb3dcb
MarilynAguilar <marilyncarolayaguilar@gmail.com>
2020-02-20 12:36:57 -0500

    Avance parte 1

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,35 @@
 const cipher = {
-  // ...
+  
+  encode: (valueNum,valueText) => 
+  {
+    //creando un ciclo for que recorra
+    for (let i = 0; i < valueText.length; i++) 
+
+    // Obteniendo la posicion del texto
+    let asciiPositionText = valueText.charCodeAt(i);
+
+    if (asciiPositionText >= 65 && asciiPositionText <= 90) 
+    {
+      //aplicando la fórmula Cifrado Cesar
+      let formulaCipher = (asciiPositionText-65+parseInt(valueNum))%26+65;
+      boxResult.value+= String.fromCharCode(formulaCipher);
+    }
+    return boxResult.value
+  },
+
+  decode:(valueNum,valueText) =>
+  {
+    for (let i = 0; i < valueText.length; i++) 
+    let asciiPositionText = valueText.charCodeAt(i);
+    if (asciiPositionText >= 65 && asciiPositionText <= 90)
+    {
+      //aplicando la fórmula cifrado Cesar
+      let formulaCipher = (asciiPositionText-90-parseInt(valueNum))%26+90;
+      boxResult.value += String.fromCharCode(formulaCipher);   
+    }
+    return boxResult.value
+  },
+
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import cipher from './cipher.js';
 
+let buttonCode = document.getElementById('buttonCode');
+let buttonDecode = document.getElementById('buttonDecode');
+let boxText = document.getElementById('boxText');
+let boxNum = document.getElementById('boxNum');
+let boxResult = document.getElementById('boxResult');
+
+buttonCode.addEventListener('click',()=>
+{  
+
+    let valueText = boxText.value.toUpperCase();
+    let valueNum = boxNum.value;
+    boxResult.value = cipher.encode(valueNum,valueText);
+})
+
+buttonDecode.addEventListener('click',() =>
+{
+    let valueText = boxText.value.toUpperCase();
+    let valueNum = boxNum.value;
+    boxResult.value = cipher.decode(valueNum,valueNum);
+    
+})
+
 console.log(cipher);
