ad7c827ea66de6c6924aab375489dc7b4e1c1d59
MarilynAguilar <marilyncarolayaguilar@gmail.com>
2020-02-21 11:32:08 -0500

    Aplicando condicional if y else

--- src/cipher.js
+++ src/cipher.js
@@ -1,32 +1,36 @@
 //prueba 
 const cipher = {
   
-  encode: (valueNum,valueText) => 
-  {
+  encode: (valueNum,valueText) => {
+
     //creando un ciclo for que recorra
     for (let i = 0; i < valueText.length; i++) 
-
-    // Obteniendo la posicion del texto
+    {
     let asciiPositionText = valueText.charCodeAt(i);
-
     if (asciiPositionText >= 65 && asciiPositionText <= 90) 
     {
-      //aplicando la fórmula Cifrado Cesar
       let formulaCipher = (asciiPositionText-65+parseInt(valueNum))%26+65;
-      boxResult.value+= String.fromCharCode(formulaCipher);
+      boxResult.value+= String.fromCharCode(formulaCipher); 
+    } else {
+      boxResult.value += valueText[i];
     }
-    return boxResult.value
+    }
+    return boxResult.value 
   },
 
   decode:(valueNum,valueText) =>
   {
-    for (let i = 0; i < valueText.length; i++) 
-    let asciiPositionText = valueText.charCodeAt(i);
-    if (asciiPositionText >= 65 && asciiPositionText <= 90)
+    
+    for (let i = 0; i < valueText.length; i++)
     {
-      //aplicando la fórmula cifrado Cesar
+      let asciiPositionText = valueText.charCodeAt(i);
+      if (asciiPositionText >= 65 && asciiPositionText <= 90)
+    {
       let formulaCipher = (asciiPositionText-90-parseInt(valueNum))%26+90;
-      boxResult.value += String.fromCharCode(formulaCipher);   
+      boxResult.value+= String.fromCharCode(formulaCipher);
+    } else {
+      boxResult.value += valueText[i];
+    }
     }
     return boxResult.value
   },
--- src/index.js
+++ src/index.js
@@ -1,26 +1,27 @@
 //prueba
 import cipher from './cipher.js';
 
-let buttonCode = document.getElementById('buttonCode');
+let buttonEncode = document.getElementById('buttonEncode');
 let buttonDecode = document.getElementById('buttonDecode');
+
 let boxText = document.getElementById('boxText');
 let boxNum = document.getElementById('boxNum');
 let boxResult = document.getElementById('boxResult');
 
-buttonCode.addEventListener('click',()=>
-{  
 
+buttonEncode.addEventListener('click',() =>
+{ 
     let valueText = boxText.value.toUpperCase();
     let valueNum = boxNum.value;
     boxResult.value = cipher.encode(valueNum,valueText);
-})
+});
 
 buttonDecode.addEventListener('click',() =>
 {
     let valueText = boxText.value.toUpperCase();
     let valueNum = boxNum.value;
-    boxResult.value = cipher.decode(valueNum,valueNum);
+    boxResult.value = cipher.decode(valueNum,valueText);
     
-})
+});
 
-console.log(cipher);
+// console.log(cipher);
